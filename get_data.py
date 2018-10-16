6ea1f4bf132e9a13c646ecf5ca52e498c10e8f05
STORMWIND\sterling.pl.blood <sterling.pl.blood@progleasing.com>
2018-10-16 16:40:35 -0600

    Added oldest and latest to paginate.

--- get_data.py
+++ get_data.py
@@ -3,17 +3,22 @@ from slackclient import SlackClient
 import markovify
 import config
 
-sc = SlackClient(config.slack_token)
-
 # underwriting: C1K9R6F7U
 # outage-management: C1KN6GBLH
 # help_environment: C59D5D7A9
-data = sc.api_call('channels.history', channel='C1K9R6F7U', count=1000)
+channel_id = 'C1K9R6F7U'
+days_of_messages = 100
+
+sc = SlackClient(config.slack_token)
+
+data = [sc.api_call('channels.history', channel=channel_id, count=1000, oldest=((x+1)*60*60*24), latest=(x*60*60*24)) for x in range(days_of_messages)]
 
 messages = []
-for msg in data['messages']:
-    if msg['text']:
-        messages.append(msg['text'])
+
+for day in data:
+    for msg in day['messages']:
+        if msg['text']:
+            messages.append(msg['text'])
 
 model = markovify.Text(". ".join(messages), state_size=2)
 
