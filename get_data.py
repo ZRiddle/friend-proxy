3f36b5a5c172e00f193ab99d9e37d1c24a3d598f
STORMWIND\sterling.pl.blood <sterling.pl.blood@progleasing.com>
2018-10-17 11:07:09 -0600

    Changed the number of days each api call gets messages from.

--- get_data.py
+++ get_data.py
@@ -7,13 +7,14 @@ import time
 # underwriting: C1K9R6F7U
 # outage-management: C1KN6GBLH
 # help_environment: C59D5D7A9
-channel_id = 'C1K9R6F7U'
-days_of_messages = 100
+channel_id = 'C1KN6GBLH'
+days_of_messages = 30
+intervals_of_days = 50
 today = time.time()
 
 sc = SlackClient(config.slack_token)
 
-data = [sc.api_call('channels.history', channel=channel_id, count=1000, oldest=(today - (x+1)*60*60*24), latest=(today - x*60*60*24)) for x in range(days_of_messages)]
+data = [sc.api_call('channels.history', channel=channel_id, count=1000, oldest=(today - (x + 1) * days_of_messages * 60 * 60 * 24), latest=(today - x * days_of_messages * 60 * 60 * 24)) for x in range(intervals_of_days)]
 
 messages = []
 
