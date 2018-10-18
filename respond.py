4a1f6a3084bb96e4cd47c9b713b7aa13da830adf
cody.austin <cody.austin@progleasing.com>
2018-10-18 12:02:49 -0600

    fix speak cache-grabbing

--- respond.py
+++ respond.py
@@ -3,6 +3,11 @@ import logging
 from cache import CACHE
 
 
-def speak(sc, channel, *args):
-    model = CACHE[channel]
+def speak(sc, channel, target=''):
+    if target[:2] == '<#':
+        model = CACHE[target.split('|')[0][2:]]
+    elif target[:2] == '<@':
+        model = CACHE[target]
+    else:
+        model = CACHE[channel]
     return model.make_short_sentence(140)
