c73ffe061badef15fd1c060026ef06e930f4e04c
Unknown <kelly.olsen@progleasing.com>
2018-10-18 10:17:35 -0600

    SPACES!!!! WHAT A WORLD
    Remove token

--- get_channel_messages.py
+++ get_channel_messages.py
@@ -1,14 +1,15 @@
 from slackclient import SlackClient
 
+
 def get_messages(channel_id):
-	token = 'xoxp-4108165166-7989336820-456462137732-0030f362b7c66e7d9b44c2d7b2c85dbc'
-	sc = SlackClient(token)
-	data = sc.api_call('channels.history', channel=channel_id, count=1000)
-	results = []
-	if data['ok'] == 'False':
-		print('no results')
-		return results
-	for msg in data['messages']:
-		if msg['text']:
-			results.append(msg['text'])
-	return results
+    token = '###'
+    sc = SlackClient(token)
+    data = sc.api_call('channels.history', channel=channel_id, count=1000)
+    results = []
+    if data['ok'] == 'False':
+        print('no results')
+        return results
+    for msg in data['messages']:
+        if msg['text']:
+            results.append(msg['text'])
+    return results
